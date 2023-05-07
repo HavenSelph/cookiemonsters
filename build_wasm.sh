391c803099dea2bc64874f431de613e62323712b
Haven Selph <havenselph@gmail.com>
2023-05-07 01:39:33 -0500

    initial commit

---
+++ build_wasm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p build
+emcc out.c -o build/index.html -sUSE_SDL_IMAGE=2 -sUSE_SDL_TTF=2 -sUSE_SDL=2 -sUSE_SDL_MIXER=2 -sUSE_SDL_GFX=2 -sALLOW_MEMORY_GROWTH=1 --preload-file assets --use-preload-plugins
